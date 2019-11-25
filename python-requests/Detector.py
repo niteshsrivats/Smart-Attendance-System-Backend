0d074f8fef53e3753f3f966cf70fde762baf70e4
Nitesh Srivats <niteshsrivats.k@gmail.com>
2019-11-25 12:12:50 +0530

    Added api calls

--- python-requests/Detector.py
+++ python-requests/Detector.py
@@ -7,3 +7,36 @@
 # also include api call to sign in, you should'nt hard cord the token
 
 # I did section.py, you can take a look otherwise this is good work
+
+
+# first v1/devices/signin
+# v1/devices - this will have sections inside
+# for each section do
+# v1/sections/sectionId/courses returns something like this
+# {
+#     "19CSE5IOT": {
+#         "name": "Nitesh",
+#         "sections": [
+#             {
+#                 "id": "5A-CSE-2019",
+#                 "section": "A",
+#                 "year": 2019,
+#                 "semester": 5
+#             }
+#         ],
+#         "courses": [
+#             {
+#                 "id": "19CSE5IOT",
+#                 "name": "Internet of Things",
+#                 "year": 2019,
+#                 "semester": 5
+#             }
+#         ],
+#         "department": {
+#             "id": "cse",
+#             "name": "Computer Science"
+#         },
+#         "id": "teacherId",
+#         "type": "TEACHER"
+#     }
+# }
